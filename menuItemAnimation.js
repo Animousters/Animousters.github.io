300e7161e3c80ca0e3b66ea49b8910a21d60f3bd
Animousters <jeffreygallegos7@gmail.com>
2023-02-07 11:05:18 -0700

    redoing stuff

--- menuItemAnimation.js
+++
@@ -1,9 +0,0 @@
-const menuItems = document.getElementById("menuItems");
-
-Array.from(document.getElementsByClassName("menuItem")).forEach(
-  (item, index) => {
-    item.onmouseover = () => {
-      menuItems.dataset.activeIndex = index;
-    };
-  }
-);
